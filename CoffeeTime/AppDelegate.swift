4d16ac5bd54604583fe6535b0ab5c50c4a064fcc
ouyangqi006 <ouyangqi006@ke.com>
2023-07-11 16:30:05 +0800

    1

--- CoffeeTime/AppDelegate.swift
+++ CoffeeTime/AppDelegate.swift
@@ -10,6 +10,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+   /*
+    1
+    */
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
