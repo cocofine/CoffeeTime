7b6265a641ff7afd0501a2929136cced0e84fd9e
ouyangqi006 <ouyangqi006@ke.com>
2023-07-11 16:32:32 +0800

    no message

--- CoffeeTime/SceneDelegate.swift
+++ CoffeeTime/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        //test3
+        
         let vc = BaseTabBarController()
         let nav = UINavigationController.init(rootViewController: vc)
         self.window?.rootViewController = nav
